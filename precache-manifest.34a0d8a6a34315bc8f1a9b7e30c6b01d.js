a31f31d7d25896c77867a7d0b853a45c07855416
Nagendra <gbrgnagendra@gmail.com>
2019-04-04 01:53:19 +0530

    Updates

---
+++ precache-manifest.34a0d8a6a34315bc8f1a9b7e30c6b01d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "abf9d6654ea683cc061a",
+    "url": "/sairam/static/js/runtime~main.11692264.js"
+  },
+  {
+    "revision": "7dde08791abbb8eb9e20",
+    "url": "/sairam/static/js/main.c064a467.chunk.js"
+  },
+  {
+    "revision": "8b7c82005d7aacfd5906",
+    "url": "/sairam/static/js/2.e4a788b2.chunk.js"
+  },
+  {
+    "revision": "7dde08791abbb8eb9e20",
+    "url": "/sairam/static/css/main.fee92777.chunk.css"
+  },
+  {
+    "revision": "ced0937466efd29520ed5c43d4ccc853",
+    "url": "/sairam/index.html"
+  }
+];
